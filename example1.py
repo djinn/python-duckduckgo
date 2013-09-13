e3a35260f5fefa2d9f996767a24c5c2273f4fe30
Supreet Sethi <supreet.sethi@gmail.com>
2013-09-14 00:28:12 +0530

    changes

--- example1.py
+++ example1.py
@@ -14,12 +14,14 @@ def build_web_tree(qr, depth=0):
         return 
     else:
          for r in ds.related:
-             if isinstance(r, Topic) == False:
+             if isinstance(r, Topic) == True:
+                 r_used = r.name.encode('ascii', 'ignore')
+             else:
                  r_used = r.text.encode('ascii', 'ignore').split('-')[0].strip()
-                 try:
-                     visited.index(r_used) 
-                 except:
-                     build_web_tree(r_used, depth=depth+1)
+             try:
+                 visited.index(r_used) 
+             except:
+                 build_web_tree(r_used, depth=depth+1)
          
 
 if __name__ == '__main__':
